c1a252b69dc5a8c0c20dd38e2ef16b271f21009f
Dickson <dk.dickson@yahoo.com>
2018-02-26 18:15:21 +0700

    add email fields to user

--- modules/backend/models/User.php
+++ modules/backend/models/User.php
@@ -22,7 +22,7 @@ class User extends UserBase
      * Validation rules
      */
     public $rules = [
-        // 'email' => 'required|between:6,255|email|unique:backend_users',
+        'email' => 'required|between:6,255|email|unique:backend_users',
         'login' => 'required|between:2,255|unique:backend_users',
         'password' => 'required:create|between:4,255|confirmed',
         'password_confirmation' => 'required_with:password|between:4,255'
